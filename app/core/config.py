f389fe62a95e36705c9fa47355e2617a4aefc74b
Lufa1u <fortyfack76@gmail.com>
2024-02-21 00:47:12 +0300

    project core

---
+++ app/core/config.py
@@ -0,0 +1,23 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+class DBConfig:
+    DB_USER = os.environ.get("POSTGRES_USER")
+    DB_PASS = os.environ.get("POSTGRES_PASSWORD")
+    DB_HOST = os.environ.get("POSTGRES_HOST")
+    DB_PORT = os.environ.get("POSTGRES_PORT")
+    DB_NAME = os.environ.get("POSTGRES_DB")
+    DB_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}"
+
+
+async def get_db():
+    engine = create_async_engine(DBConfig.DB_URL, future=True)
+    async_session = sessionmaker(bind=engine, autocommit=False, autoflush=False, class_=AsyncSession)
+    async with async_session() as session:
+        yield session
