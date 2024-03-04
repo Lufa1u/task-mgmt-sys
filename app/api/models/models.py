52eaa6d37f3d56c1aafd008f69d54bb6bea44562
Lufa1u <fortyfack76@gmail.com>
2024-03-04 21:44:44 +0300

    delete task func add

--- app/api/models/models.py
+++ app/api/models/models.py
@@ -36,7 +36,7 @@ class UserModel(Base):
     role = Column(Enum(UserRoleEnumModel), index=True, nullable=False)
 
     created_tasks = relationship("TaskModel", back_populates="creator_user")
-    assigned_tasks = relationship("TaskModel", secondary="user_task_association", back_populates="assigned_users")
+    assigned_tasks = relationship("TaskModel", secondary="user_task_association", back_populates="assigned_users", single_parent=True)
 
 
 class TaskModel(Base):
@@ -50,4 +50,4 @@ class TaskModel(Base):
     creator_user_id = Column(Integer, ForeignKey("users.id"))
     creator_user = relationship("UserModel", back_populates="created_tasks")
 
-    assigned_users = relationship("UserModel", secondary="user_task_association", back_populates="assigned_tasks")
+    assigned_users = relationship("UserModel", secondary="user_task_association", back_populates="assigned_tasks", single_parent=True)
