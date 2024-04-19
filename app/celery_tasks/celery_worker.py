af1ec9857a617e80aefc37dcadfdf867f4ad0d97
Lufa1u <fortyfack76@gmail.com>
2024-04-19 23:32:46 +0300

    celery_worker created

--- app/celery_tasks/celery_worker.py
+++ app/celery_tasks/celery_worker.py
@@ -1,6 +1,8 @@
 from celery import Celery
 
 
-celery_app = Celery("tasks", broker="amqp://guest:guest@localhost:5672/")
-
+celery_app = Celery("celery_app")
+celery_app.conf.broker_connection_retry_on_startup = True
+celery_app.conf.broker_url = "amqp://guest:guest@localhost:5672"
+celery_app.conf.result_backend = "rpc://"
 
