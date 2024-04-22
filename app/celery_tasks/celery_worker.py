3bbff5dec4ba5855665b18258a0f6da7538a8ac6
Lufa1u <fortyfack76@gmail.com>
2024-04-23 01:08:37 +0300

    docker files fix

--- app/celery_tasks/celery_worker.py
+++ app/celery_tasks/celery_worker.py
@@ -2,5 +2,7 @@ from celery import Celery
 
 
 celery_app = Celery("celery_app")
-celery_app.config_from_object("app.celery_tasks.config")
+celery_app.conf.update(broker_url="amqp://guest:guest@rabbitmq:5672",
+                       result_backend="rpc://",
+                       broker_connection_retry_on_startup=True)
 
